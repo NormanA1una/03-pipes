89b4d1029e445d0615fefa66bbfbaf81737da5d2
NormanA1una <baciloferoz@gmail.com>
2023-05-19 16:51:45 -0600

    wip: diferents pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 
 export interface PeriodicElement {
-  variable: string;
+  variable: any;
   pipe: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
   { variable: 'Capitan América', pipe: 'Uppercase' },
   { variable: 'Capitan América', pipe: 'Lowercase' },
+  { variable: 'Capitan América', pipe: 'Slice:3' },
+  { variable: 'Capitan América', pipe: 'Slice:0:3' },
+  { variable: [1, 2, 3, 4, 5, 6, 7], pipe: 'Slice:1:5' },
+  { variable: Math.PI, pipe: 'number:1.0-0' },
+  { variable: Math.PI, pipe: 'number:.0-2' },
+  { variable: 0.234, pipe: 'percent' },
+  { variable: 0.234, pipe: 'percent:2.2-2' },
+  { variable: 1234.5, pipe: 'currency' },
+  { variable: 1234.5, pipe: 'currency:EUR' },
+  { variable: 1234.5, pipe: 'currency:CAD:symbol:.0-0' },
 ];
 
 @Component({
